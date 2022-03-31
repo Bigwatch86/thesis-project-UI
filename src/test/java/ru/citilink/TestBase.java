a8de3e6c670ec894f7727f2bf59ded2c44ded17c
Bigwatch86 <bigwatch86@gmail.com>
2022-03-31 16:34:49 +0300

    added tests

---
+++ src/test/java/ru/citilink/TestBase.java
@@ -0,0 +1,45 @@
+package ru.citilink;
+
+import com.codeborne.selenide.Configuration;
+import helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+
+public class TestBase {
+    @BeforeAll
+    static void setUp() {
+        String browser = System.getProperty("browser");
+        String version = System.getProperty("version");
+        String remoteUrl = System.getProperty("remoteUrl");
+        String login = System.getProperty("login");
+        String password = System.getProperty("password");
+
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = browser;
+        Configuration.browserVersion = version;
+        Configuration.remote = "https://" + login + ":" + password + "@" + remoteUrl;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+    @BeforeEach
+    void baseURL(){
+        open("https://www.citilink.ru/");
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
---
+++ src/test/java/ru/citilink/Tests.java
@@ -0,0 +1,77 @@
+package ru.citilink;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static io.qameta.allure.Allure.step;
+
+public class Tests extends TestBase{
+
+    @Test
+    @Owner("igor.glazov")
+    @Feature("Citilink")
+    @DisplayName("Проверка загрузки header")
+    @Severity(SeverityLevel.BLOCKER)
+    public void headerLoading() {
+        step("Проверяем загрузку header", () -> {
+            $(".Container").shouldBe(visible);
+            $(".Container .MainHeader__logo").shouldBe(visible);
+        });
+    }
+
+    @Test
+    @Owner("igor.glazov")
+    @Feature("Citilink")
+    @DisplayName("Специальный падающий тест")
+    @Severity(SeverityLevel.BLOCKER)
+    public void searchWishlist(){
+        step("Проверяем наличие избранного", () -> {
+            $(".HeaderMenu__buttons_wishlist").shouldBe(visible);
+            $(".HeaderMenu__buttons_wishlist").click();
+        });
+        step("Нет избранного, так как пользователь не залогинен", () -> {
+            $("h2").shouldHave(text("Избранное"));
+        });
+    }
+
+    @Test
+    @Owner("igor.glazov")
+    @Feature("Citilink")
+    @DisplayName("Проверка наличия корзины")
+    @Severity(SeverityLevel.BLOCKER)
+    public void searchBasket(){
+        step("Проверяем наличие корзины", () -> {
+            $(".HeaderMenu__buttons_basket").shouldBe(visible);
+        });
+    }
+
+    @Test
+    @Owner("igor.glazov")
+    @Feature("Citilink")
+    @DisplayName("Проверка наличия каталога товаров")
+    @Severity(SeverityLevel.BLOCKER)
+    public void catalogIsVisible(){
+        step("Проверяем наличие каталога товаров", () -> {
+            $("[data-label=\"Каталог товаров\"]").shouldBe(visible);
+        });
+    }
+
+    @Test
+    @Owner("igor.glazov")
+    @Feature("Citilink")
+    @DisplayName("Проверка загрузки каталога товаров")
+    @Severity(SeverityLevel.BLOCKER)
+    public void catalogLoading(){
+        step("Проверяем загрузку каталога товаров", () -> {
+            $("[data-label=\"Каталог товаров\"]").click();
+            $(".CatalogMenu__category").shouldBe(visible);
+        });
+    }
+}
